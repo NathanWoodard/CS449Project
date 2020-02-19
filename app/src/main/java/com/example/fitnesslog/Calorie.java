8480e36fedc8ee894bc9ee0da7ccebf46bcb64b1
Nathan Woodard <nathanphilipwoodard@gmail.com>
2020-02-19 13:17:23 -0600

    first commit

---
+++ app/src/main/java/com/example/fitnesslog/Calorie.java
@@ -0,0 +1,14 @@
+package com.example.fitnesslog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calorie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie);
+    }
+}
---
+++ app/src/main/java/com/example/fitnesslog/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.fitnesslog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    Button calorieButton;
+    Button workoutButton;
+    Button sleepButton;
+    Button weightButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        calorieButton = findViewById(R.id.calorie);
+        calorieButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openCalorie();
+            }
+        });
+        workoutButton = findViewById(R.id.workout);
+        workoutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openWorkout();
+            }
+        });
+        sleepButton = findViewById(R.id.sleep);
+        workoutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openSleep();
+            }
+        });
+        weightButton = findViewById(R.id.weight);
+        workoutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openWeight();
+            }
+        });
+    }
+    public void openCalorie(){
+        Intent intent = new Intent(this, Calorie.class);
+        startActivity(intent);
+    }
+    public void openWorkout(){
+        Intent intent = new Intent(this, Workout.class);
+        startActivity(intent);
+    }
+    public void openSleep(){
+        Intent intent = new Intent(this, Sleep.class);
+        startActivity(intent);
+    }
+    public void openWeight(){
+        Intent intent = new Intent(this, Weight.class);
+        startActivity(intent);
+    }
+}
