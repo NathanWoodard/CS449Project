a216d8dfa07527231f1a697f1b681b50ea4470d6
Nathan Woodard <nathanphilipwoodard@gmail.com>
2020-03-28 22:31:49 -0500

    Added calorie log page with SCROLLING VIEWS

--- app/src/main/java/com/example/fitnesslog/Calorie.java
+++ app/src/main/java/com/example/fitnesslog/Calorie.java
@@ -3,6 +3,7 @@ package com.example.fitnesslog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Environment;
 import android.view.MenuItem;
@@ -42,19 +43,17 @@ public class Calorie extends AppCompatActivity {
         addCalorieButton = findViewById(R.id.addcalbutton);
         currentCalorieText = findViewById(R.id.textView4);
         addCalorieEdit = findViewById(R.id.editText);
-        addCalorieButton.setOnClickListener(new View.OnClickListener(){
+        addCalorieButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if(currentCalorieText.getVisibility()==View.VISIBLE){
+            public void onClick(View v) {
+                if (currentCalorieText.getVisibility() == View.VISIBLE) {
                     currentCalorieText.setVisibility(View.GONE);
                     addCalorieEdit.setVisibility(View.VISIBLE);
                     addCalorieButton.setText("CONFIRM");
-                }
-                else{
+                } else {
                     try {
                         currentCalorieText.setText(String.valueOf(Integer.parseInt(currentCalorieText.getText().toString()) + Integer.parseInt(addCalorieEdit.getText().toString())));
-                    }
-                    catch(NumberFormatException e){
+                    } catch (NumberFormatException e) {
                         //does nothing if the user does not input a number at all cause for some reason java doesnt interpret an empty string as 0 when you parseint it
                     }
                     currentCalorieText.setVisibility(View.VISIBLE);
@@ -66,25 +65,24 @@ public class Calorie extends AppCompatActivity {
         });
 
         resetButton = findViewById(R.id.resetbutton);
-        resetButton.setOnClickListener(new View.OnClickListener(){
+        resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 currentCalorieText.setText("0");
             }
         });
         maxCalorieEdit = findViewById(R.id.editText2);
         maxCalorieText = findViewById(R.id.textView3);
         maxCalorieButton = findViewById(R.id.maxcalbutton);
-        maxCalorieButton.setOnClickListener(new View.OnClickListener(){
+        maxCalorieButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(maxCalorieText.getVisibility()==View.VISIBLE){
+                if (maxCalorieText.getVisibility() == View.VISIBLE) {
                     maxCalorieText.setVisibility(View.GONE);
                     maxCalorieEdit.setVisibility(View.VISIBLE);
                     maxCalorieButton.setText("CONFIRM");
 
-                }
-                else{
+                } else {
                     maxCalorieText.setText(maxCalorieEdit.getText().toString());
                     maxCalorieText.setVisibility(View.VISIBLE);
                     maxCalorieEdit.setVisibility(View.GONE);
@@ -98,27 +96,30 @@ public class Calorie extends AppCompatActivity {
         mTextView = findViewById(R.id.textView4);
     }
 
-    private void saveData(){
+    private void saveData() {
         SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.putInt("currentCalorie", Integer.parseInt(currentCalorieText.getText().toString()));
         editor.putInt("maxCalorie", Integer.parseInt(maxCalorieText.getText().toString()));
         editor.commit();
     }
-    private void loadData(){
+
+    private void loadData() {
         SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         currentCalorieText.setText(String.valueOf(sp.getInt("currentCalorie", 0)));
         maxCalorieText.setText(String.valueOf(sp.getInt("maxCalorie", 0)));
     }
-    public boolean onOptionsItemSelected(MenuItem item){
+
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if(id == android.R.id.home){
+        if (id == android.R.id.home) {
             this.finish();
         }
         return super.onOptionsItemSelected(item);
     }
-    public void save(View v){
+
+    public void save(View v) {
         String text = mTextView.getText().toString();
         FileOutputStream fos = null;
 
@@ -128,7 +129,7 @@ public class Calorie extends AppCompatActivity {
 
         try {
             Calendar c = Calendar.getInstance();
-            fos = openFileOutput(FILE_NAME , MODE_APPEND);
+            fos = openFileOutput(FILE_NAME, MODE_APPEND);
             fos.write(text.getBytes());
             text = "\n";
             fos.write(text.getBytes());
@@ -141,8 +142,8 @@ public class Calorie extends AppCompatActivity {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally{
-            if(fos != null){
+        } finally {
+            if (fos != null) {
                 try {
                     fos.close();
                 } catch (IOException e) {
@@ -150,6 +151,11 @@ public class Calorie extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    public void openCalorieLog(View v) {
+    Intent intent = new Intent(this, CalorieLog.class);
+    startActivity(intent);
     }
     @Override
     protected void onPause(){
---
+++ app/src/main/java/com/example/fitnesslog/CalorieLog.java
@@ -0,0 +1,135 @@
+package com.example.fitnesslog;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CalorieLog extends AppCompatActivity {
+    TextView[] test = new TextView[100];
+    private static final String FILE_NAME = "calorie.txt";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie_log);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ConstraintLayout scroll = findViewById(R.id.info);
+        ConstraintSet c = new ConstraintSet();
+
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            // StringBuilder sb = new StringBuilder();
+            String day, calories;
+            int i = 0;
+            while ((calories = br.readLine()) != null) {
+                day = br.readLine();
+                if(i !=0 && test[i - 2].getText().toString().equals(day)){
+                    test[i-1].setText(String.valueOf(Integer.parseInt(test[i-1].getText().toString()) + Integer.parseInt(calories)));
+                }
+                else{
+                    TextView day_view = new TextView(this);
+                    day_view.setText(day);
+                    day_view.setId(View.generateViewId());
+                    ConstraintLayout.LayoutParams day_params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                    day_view.setLayoutParams(day_params);
+                    day_view.setTextSize(30);
+                    scroll.addView(day_view);
+                    test[i] = day_view;
+                    TextView cal_view = new TextView(this);
+                    cal_view.setText(calories);
+                    cal_view.setId(View.generateViewId());
+                    ConstraintLayout.LayoutParams cal_params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                    cal_view.setLayoutParams(cal_params);
+                    cal_view.setTextSize(30);
+                    scroll.addView(cal_view);
+                    test[i+1] = cal_view;
+                    i+=2;
+                }
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        /*for(int i=0;i<100;i++){
+            TextView temp = new TextView(this);
+            temp.setText("1");
+            temp.setId(View.generateViewId());
+            temp.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
+            scroll.addView(temp);
+            test[i] = temp;
+
+
+        }
+        */
+        c.clone(scroll);
+        c.connect(test[0].getId(), ConstraintSet.TOP, R.id.info, ConstraintSet.TOP);
+        c.connect(test[0].getId(), ConstraintSet.LEFT, R.id.info, ConstraintSet.LEFT);
+        c.connect(test[1].getId(), ConstraintSet.TOP, R.id.info, ConstraintSet.TOP);
+        c.connect(test[1].getId(), ConstraintSet.RIGHT, R.id.info, ConstraintSet.RIGHT);
+        for(int i = 2;i<100;i+=2){
+            if(test[i]==null){
+                break;
+            }
+            c.connect(test[i].getId(), ConstraintSet.TOP, test[i-2].getId(), ConstraintSet.BOTTOM);
+            c.connect(test[i].getId(), ConstraintSet.START, R.id.info, ConstraintSet.START);
+            c.connect(test[i+1].getId(), ConstraintSet.TOP, test[i-2].getId(), ConstraintSet.BOTTOM);
+            c.connect(test[i+1].getId(), ConstraintSet.END, R.id.info, ConstraintSet.END);
+        }
+        c.applyTo(scroll);
+
+    }
+
+    /*public TextView[] load() {
+        FileInputStream fis = null;
+        TextView[] logs;
+        try {
+            fis = openFileInput("calorie.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            // StringBuilder sb = new StringBuilder();
+            String day, calories;
+
+            while ((calories = br.readLine()) != null) {
+                day = br.readLine();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }*/
+
+}
