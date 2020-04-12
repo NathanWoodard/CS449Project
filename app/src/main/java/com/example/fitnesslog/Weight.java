b870798a95b41f3908291dbcd0411f2d49fb3bf3
jjbmm4 <42724399+jjbmm4@users.noreply.github.com>
2020-04-11 21:49:29 -0500

    Creating Weight
    creating weight log

--- app/src/main/java/com/example/fitnesslog/Weight.java
+++ app/src/main/java/com/example/fitnesslog/Weight.java
@@ -2,13 +2,140 @@ package com.example.fitnesslog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Weight extends AppCompatActivity {
+
+    //File Stuff
+    private static final String FILE_NAME = "weight.txt";
+    TextView mTextView;
+    //END File Stuff
+
+    Button setWeight;
+    Button submitWeight;
+    Button weightLog;
+
+    TextView currentWeightInput;
+
+    EditText userEditWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight);
+        mTextView = findViewById(R.id.CurrentWeightInput);
+
+        setWeight = findViewById(R.id.SetWeight);
+        submitWeight = findViewById(R.id.SubmitWeight);
+        weightLog = findViewById(R.id.WeightLog);
+
+        currentWeightInput = findViewById(R.id.CurrentWeightInput);
+
+        userEditWeight = (findViewById(R.id.UserEditWeight));
+
+        setWeight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentWeightInput.getVisibility() == View.VISIBLE) {
+                    currentWeightInput.setVisibility(View.GONE);
+                    userEditWeight.setVisibility(View.VISIBLE);
+                    setWeight.setText("CONFIRM");
+                }
+                else {
+                    try {
+                        currentWeightInput.setText(String.valueOf(Integer.parseInt(userEditWeight.getText().toString())));
+                    } catch (NumberFormatException e) {
+                        //does nothing if the user does not input a number at all cause for some reason java doesnt interpret an empty string as 0 when you parseint it
+                    }
+                    currentWeightInput.setVisibility(View.VISIBLE);
+                    userEditWeight.getText().clear();
+                    userEditWeight.setVisibility(View.GONE);
+                    setWeight.setText("SET WEIGHT");
+                }
+            }
+        });
+
     }
+
+
+    public void save(View v) {
+        String text = mTextView.getText().toString();
+        FileOutputStream fos = null;
+
+        mTextView.setText("0");
+        Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                Toast.LENGTH_LONG).show();
+
+        try {
+            Calendar c = Calendar.getInstance();
+            fos = openFileOutput(FILE_NAME, MODE_APPEND);
+            fos.write(text.getBytes());
+            text = "\n";
+            fos.write(text.getBytes());
+            String dayLongName = c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+            fos.write(dayLongName.getBytes());
+            text = "\n";
+            fos.write(text.getBytes());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    private void saveData() {
+        SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("currentWeightInput", Integer.parseInt(currentWeightInput.getText().toString()));
+        editor.commit();
+    }
+
+    private void loadData() {
+        SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        currentWeightInput.setText(String.valueOf(sp.getInt("currentWeightInput", 0)));
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        saveData();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        loadData();
+    }
+
+    public void openWeightLog(View v){
+        Intent intent = new Intent(this, WeightLog.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/fitnesslog/WeightLog.java
@@ -0,0 +1,96 @@
+package com.example.fitnesslog;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WeightLog extends AppCompatActivity {
+    TextView[] test = new TextView[100];
+    private static final String FILE_NAME = "weight.txt";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_log);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ConstraintLayout scroll = findViewById(R.id.info);
+        ConstraintSet c = new ConstraintSet();
+
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            // StringBuilder sb = new StringBuilder();
+            String day, weight;
+            int i = 0;
+            while ((weight = br.readLine()) != null) {
+                day = br.readLine();
+                if(i !=0 && test[i - 2].getText().toString().equals(day)){
+                    test[i-1].setText(String.valueOf(Integer.parseInt(test[i-1].getText().toString()) + Integer.parseInt(weight)));
+                }
+                else{
+                    TextView day_view = new TextView(this);
+                    day_view.setText(day);
+                    day_view.setId(View.generateViewId());
+                    ConstraintLayout.LayoutParams day_params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                    day_view.setLayoutParams(day_params);
+                    day_view.setTextSize(30);
+                    scroll.addView(day_view);
+                    test[i] = day_view;
+                    TextView cal_view = new TextView(this);
+                    cal_view.setText(weight);
+                    cal_view.setId(View.generateViewId());
+                    ConstraintLayout.LayoutParams cal_params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                    cal_view.setLayoutParams(cal_params);
+                    cal_view.setTextSize(30);
+                    scroll.addView(cal_view);
+                    test[i+1] = cal_view;
+                    i+=2;
+                }
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        c.clone(scroll);
+        c.connect(test[0].getId(), ConstraintSet.BOTTOM, R.id.info, ConstraintSet.BOTTOM);
+        c.connect(test[0].getId(), ConstraintSet.LEFT, R.id.info, ConstraintSet.LEFT);
+        c.connect(test[1].getId(), ConstraintSet.BOTTOM, R.id.info, ConstraintSet.BOTTOM);
+        c.connect(test[1].getId(), ConstraintSet.RIGHT, R.id.info, ConstraintSet.RIGHT);
+        for(int i = 2;i<100;i+=2){
+            if(test[i]==null){
+                break;
+            }
+            c.connect(test[i].getId(), ConstraintSet.BOTTOM, test[i-2].getId(), ConstraintSet.TOP);
+            c.connect(test[i].getId(), ConstraintSet.START, R.id.info, ConstraintSet.START);
+            c.connect(test[i+1].getId(), ConstraintSet.BOTTOM, test[i-2].getId(), ConstraintSet.TOP);
+            c.connect(test[i+1].getId(), ConstraintSet.END, R.id.info, ConstraintSet.END);
+        }
+        c.applyTo(scroll);
+
+    }
+
+
+
+}
